d805512885e6dd760e3dd5d18a3efca7dfe57c22
vineshsalakau <lohemguy28@gmail.com>
2026-05-15 13:02:50 +0800

    Update ModEntities.java

--- parched-mod/src/main/java/com/parchedmod/init/ModEntities.java
+++ parched-mod/src/main/java/com/parchedmod/init/ModEntities.java
@@ -6,17 +6,24 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModEntities {
 
+    public static final RegistryKey<EntityType<?>> PARCHED_KEY = RegistryKey.of(
+            RegistryKeys.ENTITY_TYPE,
+            Identifier.of("parched", "parched")
+    );
+
     public static final EntityType<ParchedEntity> PARCHED = Registry.register(
             Registries.ENTITY_TYPE,
-            Identifier.of("parched", "parched"),
+            PARCHED_KEY,
             EntityType.Builder.create(ParchedEntity::new, SpawnGroup.MONSTER)
-                    .dimensions(0.6F, 1.99F)      // Same hitbox as skeleton
+                    .dimensions(0.6F, 1.99F)
                     .maxTrackingRange(8)
-                    .build()
+                    .build(PARCHED_KEY)
     );
 
     public static void register() {
