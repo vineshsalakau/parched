9b5d72471dd52d7876368e65271d5b3a3cf36ab9
vineshsalakau <lohemguy28@gmail.com>
2026-05-15 13:13:57 +0800

    Update ParchedEntityRenderer.java

--- parched-mod/src/client/java/com/parchedmod/render/ParchedEntityRenderer.java
+++ parched-mod/src/client/java/com/parchedmod/render/ParchedEntityRenderer.java
@@ -5,13 +5,6 @@ import net.minecraft.client.render.entity.SkeletonEntityRenderer;
 import net.minecraft.entity.mob.SkeletonEntity;
 import net.minecraft.util.Identifier;
 
-/**
- * Uses the standard skeleton model but with the parched texture.
- * Parched shares the exact same skeleton geometry in vanilla 1.21.11.
- *
- * Texture location: src/main/resources/assets/parched/textures/entity/parched.png
- * See README for instructions on obtaining the texture.
- */
 public class ParchedEntityRenderer extends SkeletonEntityRenderer {
 
     private static final Identifier TEXTURE =
@@ -21,7 +14,6 @@ public class ParchedEntityRenderer extends SkeletonEntityRenderer {
         super(context);
     }
 
-    @Override
     public Identifier getTexture(SkeletonEntity entity) {
         return TEXTURE;
     }
